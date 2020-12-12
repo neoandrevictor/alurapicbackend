2881f4e1c7983aa88b2e4180e34f0721dde8a68e
neoandrevictor <neoandrevictor@hotmail.com>
2020-12-12 20:04:27 -0300

    aula 1

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,6 @@
+angular.module('alurapic').controller('FotosController', function ($scope) {
+    $scope.foto = {
+        titulo: 'Leão',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Lion_%28Panthera_leo%29_%2830941994012%29.jpg/800px-Lion_%28Panthera_leo%29_%2830941994012%29.jpg'
+    }
+})
